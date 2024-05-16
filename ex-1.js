fc59f6c3f2176679190a05c4e26d0be847a803b1
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-16 16:54:12 +0700

    feat:did some Loop JS homework

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,8 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
+let studentScores = [89, 70, 60, 50, 100, 71];
+for (let i = 0; i < studentScores.length; i++) {
+    studentScores[i] -= 10;
+}
+console.log(studentScores);
--- ex-2.js
+++ ex-2.js
@@ -1,6 +1,8 @@
 // Exercise #2: Sum the Numbers
-let numbers = [10, 20, 30, 40, 50];
-let total = 0;
-// Start coding here
+let productPrices = [10, 20, 30, 40, 50];
+let totalPrice = 0;
 
-console.log(total);
+for (let i = 0; i < productPrices.length; i++) {
+    totalPrice += productPrices[i];
+}
+console.log(totalPrice);
