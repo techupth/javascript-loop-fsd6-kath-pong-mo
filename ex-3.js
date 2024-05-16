8ccaaa8394cf637028b3aecd10be1fa0c76aecf9
Chavanvit <chavanvit.n@gmail.com>
2024-05-16 15:28:34 +0700

    fead:Add JS loop ex.1-6

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,18 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+minScore = scores[0];
+
+// for (let checkMin of scores) {
+//   if (checkMin < minScore) {
+//     minScore = checkMin;
+//   }
+// }
+
+for (let i = 0; i < scores.length; i++) {
+  if (minScore > scores[i]) {
+    minScore = scores[i];
+  }
+}
 
 console.log(minScore);
